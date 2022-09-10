85a7b6038f5be9fc628011902deed57f1ff5958f
Lachlan.McDermott <Lachlan.McDermott@GMR-118-1-3>
2022-09-10 10:33:14 -0700

    Add project files.

---
+++ AVLTreePractice/AVLTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTreePractice
+{
+    class Tree<T> where T : IComparable<T>
+    {
+        Node<T> Head;
+
+        public void Insert(T value)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(value, null);
+                return;
+            }
+
+            Node<T> current = Head;
+            current.Parent.Height++;
+
+            Balance(current);
+        }
+
+        public void Balance(T node)
+        {
+
+        }
+
+        public void Rotate(T node)
+        {
+
+        }
+
+    }
+}
---
+++ AVLTreePractice/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVLTreePractice
+{
+    class Node<T> where T : IComparable<T>
+    {
+        public T Value;
+        public int Height;
+        public Node<T> Left;
+        public Node<T> Right;
+        public Node<T> Parent;
+
+        public bool isLeaf
+        {
+            get
+            {
+                return this.Left == null && this.Right == null;
+            }
+        }
+
+        public bool isLeftChild
+        {
+            get
+            {
+                return this.Parent.Left == this;
+            }
+        }
+
+        public Node(T value, Node<T> parent)
+        {
+            Height = 1;
+            Value = value;
+            Left = null;
+            Right = null;
+            Parent = parent;
+        }
+    }
+}
